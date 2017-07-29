05a760b1c0eea2965fda22358e537d772962d064
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2017-07-29 19:20:37 +0530

    number
    number

---
+++ number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main() 
+{
+int n;
+clrscr;
+printf("Enter a number:");
+scanf("%d",&n);
+if(n>=1)
+{
+printf("It is POSITIVE Number:%d",a);
+}
+else if(n==0) 
+{
+printf("It is a ZERO:%d",a);
+}
+else
+{
+printf("It is NEGATIVE Number:%d",a);
+}
+getch();
+}
