0919396e9e5d2ec250e666ad2bd572e6f9b7eb7b
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2017-07-29 19:47:42 +0530

    od
    od

---
+++ od.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+int n;
+clrscr;
+printf("Enter a number:");
+scanf("%d",&n);
+if(n/2==0)
+{
+printf("It is EVEN Number:%d",n);
+}
+else
+{
+printf("It is ODD Number:%d",n);
+}
+getch();
+}
