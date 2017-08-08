10ba534c101980c4231e60409858cb5452535313
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2017-08-08 15:55:27 +0530

    Update cons.c

--- cons.c
+++ cons.c
@@ -6,8 +6,8 @@ int lcase,ucase;
 clrscr();
 printf("Enter the Alphabets");
 scanf("%d",&s);
-lcase=(s=='A'||s=='E'||s=='I'||s=='O'||s=='U')
-ucase=(s=='a'||s=='e'||s=='i'||s=='o'||s=='u')
+lcase=(s=='A'||s=='E'||s=='I'||s=='O'||s=='U');
+ucase=(s=='a'||s=='e'||s=='i'||s=='o'||s=='u');
 if(lcase||ucase)
 {
 printf("It is Vowel");
