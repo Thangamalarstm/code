d375c80628fba05d638f868fa32bec6780c2082f
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2017-08-08 16:15:22 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+int at;
+clrscr;
+printf("Enter a number:");
+scanf("%d",&at);
+if(at/4==0)
+{
+printf("It is a Leap year:%d",at);
+}
+else
+{
+printf("It is not a Leap year:%d",at);
+}
+getch();#
