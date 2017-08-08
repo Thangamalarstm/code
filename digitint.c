398a4905f83708b7457ff687c8da4f6bfa058b48
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2017-08-08 16:50:11 +0530

    Create digitint.c

---
+++ digitint.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+int n=0;
+long  c;
+clrscr;
+printf("Enter a number:");
+scanf("%1d",&n);
+while(n!=0)
+{
+c++;
+n/=10
+}
+printf("The sum of natural number:%ld",c);
+getch();
+}
