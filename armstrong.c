7d1dc9ddf7860a1d73a8f549f17eb240bbf9973a
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2017-08-29 15:12:20 +0530

    hkjk
    fjj

---
+++ armstrong.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int n,a.b,c=0;
+printf("Enter the number");
+scanf("%d",&b);
+a=n;
+while(a!=n)
+{
+b=a%10;
+c+=b*b*b;
+a/=10;
+}
+if(c==10)
+{
+printf("%d is a armstrong number",n);
+}
+else
+{
+printf("%d is not a armstrong number",n);
+}
+return 0;
+}
+
