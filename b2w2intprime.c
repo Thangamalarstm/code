435400762f278ab67d8e9a3d68a848903897b2db
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2017-08-29 15:58:31 +0530

    Create b2w2intprime.c

---
+++ b2w2intprime.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int l,h,c,i;
+printf("/n Enter the two number");
+scanf("%d",&l,&h);
+printf("prime number b/w %d and %d:"l,h);
+}
+while(l<h)
+{
+c=0
+}
+for(i=2;i<=l/2;++i)
+{
+if(l%i==0)
+{
+c=1;
+break;
+}
+}
+if(c==0)
+{
+printf("%d is a prime number:",l);
+++l;
+}
+return 0;
+}
