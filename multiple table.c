3a89adab319ecacf25bac87cee04505c0a4a8d44
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2017-08-29 15:50:39 +0530

    Create multiple table.c

---
+++ multiple table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() 
+{
+int i,n,c;
+printf("Enter a number:");
+scanf("%d",&n);
+for(i=1;i<=10;i++)
+{
+c=n*i;
+}
+printf("The multiplication table is:%d*%d=%d",n,i,c);
+return 0;
+}
