92e78471c3c3c86a3a9ebaadf73baf45161b1f3f
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-01-10 16:16:02 +0530

    Update max.c

--- max.c
+++ max.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 main()
 {
-int a[25],i,m=o,n;
+int a[25],i,m=0,n;
 printf("Enter the required number");
 scanf("%d",&n);
 for(i=0;i<=n;i++)
@@ -9,10 +9,13 @@ for(i=0;i<=n;i++)
 printf("enter the value");
   scanf("%d",&a[i]);
 }
+  for(i=0;i<=n;i++)
+  {
 if(m<a[i])
 {
 m=a[i];
 }
-  printf("maximum element is %d",a[i]);
+  }
+  printf("maximum element is %d",m);
 }
 
