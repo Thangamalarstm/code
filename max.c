dbb8fd7772e26147979b35142a1fe28678296328
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-01-10 16:05:00 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+int a[25],i,m,n;
+printf("Enter the required number");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+printf("a[%d]=",a[i]);
+}
+if(m<a[i])
+{
+m=a[i];
+}
+}
+
