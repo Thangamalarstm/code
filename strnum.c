7b05dd570d74bcb53ffd9e20bc44580e4e53af12
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-01-17 16:09:22 +0530

    Create strnum.c

---
+++ strnum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() 
+{
+int n;
+printf("Enter a number:");
+scanf("%d",&n);
+if(isdigit(n))
+{
+print("It is a num:%d");
+}
+else
+{
+printf("It is not a Number);
+}
+return 0;
+}
