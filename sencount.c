1e1331f925d8c043c45c077b947e55c04620f796
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-01-31 15:57:01 +0530

    Create sencount.c

---
+++ sencount.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i,ic;
+char a[100]="good.best;
+printf("Enter the sentence:");
+scanf("%d",&a[i]);
+for(i=1;a[i]='/0';i++)
+{
+if(a[i]==".")
+ic++;
+}
+printf("Count of the given sentence is:%d",ic+1);
+return 0;
+}
