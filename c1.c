22f2486420fe438d6f54b73256ca406aac50a1e4
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-01-31 16:23:33 +0530

    Create c1.c

---
+++ c1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+char s[200];
+int i,c=0;
+clrscr();
+printf("Enter the Alphabets");
+scanf("%s",s);
+for(i=1;s[i]<='\0';i++)
+{
+if(s[i]==' ')
+{
+printf("The count is..%s",c+1);
+c++;
+}
+}
+
+}
