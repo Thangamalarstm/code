8bc5b79f7d67c02fe0ea6afd3d2051cbd887641a
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-01-31 16:37:28 +0530

    Create a2.c

---
+++ a2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+   char a[100], c;
+   int i;
+
+   printf("Enter a string: ");
+   scanf();
+
+   printf("Enter a character: ");
+   scanf("%s",&c);
+
+   for(i = 0;a[i] != '\0'; ++i)
+   {
+       if(c == str[i])
+          
+   }
+
+   printf(" %s = %d", c);
+
+   return 0;
+}
