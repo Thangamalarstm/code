0e3e213d1c59a4521f31bfe17f9d846ccae00013
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-02-02 15:45:31 +0530

    Create strcatwostrconcept.c

---
+++ strcatwostrconcept.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char a[25],b[40];
+printf("Enter the string");
+scanf("%s",a);
+printf("Enter the string");
+scanf("%s",b);
+printf("The string is....: %s%s",a,b);
+return 0;
+}
