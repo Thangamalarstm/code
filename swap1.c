b638fc5d562177d6954ae0ac2d9c4ba9b2c21758
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-02-05 15:43:36 +0530

    Create swap1.c

---
+++ swap1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() 
+{
+int u,s,tmp;
+printf("\n Enter s&u numbers:");
+scanf("%d%d",&a,&b);
+tmp=s;
+s=u;
+u=tmp;
+printf("\n After Swapping of numbers :%d %d",s,u);
+return 0;
+}
