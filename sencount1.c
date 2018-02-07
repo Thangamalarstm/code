75e4150750914501d72a350866c698da88988f0d
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-02-07 16:15:01 +0530

    Rename sencount.c to sencount1.c

---
+++ sencount1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i,c;
+char a[100]="good.best;
+printf("Enter the sentence:");
+scanf("%d",&a[i]);
+for(i=1;a[i]='/0';i++)
+{
+if(a[i]==".")
+c++;
+}
+printf("Count of the given sentence is:%d",c+1);
+return 0;
+}
