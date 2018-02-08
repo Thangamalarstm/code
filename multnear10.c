80e05a691d25fe17eab7d51f77773d2ff80eb683
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-02-08 22:30:19 +0530

    Create multnear10.c

---
+++ multnear10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int s,a,b,c;
+printf("Enter the number..:");
+scanf("%d",&s);
+a=s/10;
+b=a+1;
+c=b*10;
+printf("The nearest greatest muliple..:%d",c);
+return 0;
+}
