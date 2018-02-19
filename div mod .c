3a5b96c76775d83f81f083752941acb15989afa1
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-02-19 16:14:21 +0530

    Create div mod .c

---
+++ div mod .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d;
+printf("\n Enter the 1st number ");
+scanf("%d",&a);
+printf("\n Enter the 2nd number");
+scanf("%d",&b);
+c=a/b;
+d=a%b;
+printf("\n %d",c);
+printf("\n %d",d);
+return 0;
+}
