e5c60a32884655b4e99ac7a66d89d092cec10c9e
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-02-19 16:05:46 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float a,b,c;
+printf("\n Length of an area");
+scanf("%f",&a);
+printf("\n Breath of an area");
+scanf("%f",&b);
+c=b*a;
+printf("%5f",c);
+return 0;
+}
