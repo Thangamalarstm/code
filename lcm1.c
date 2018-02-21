c526b807f62b78901e69e5792c36d3555425fe08
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-02-21 16:56:54 +0530

    Create lcm1.c

---
+++ lcm1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+    int main()
+    {
+  int a,b,s;
+  printf("Enter the two values");
+   scanf("%d%d",&a,&b);
+   s=(a<b)?a:b;
+   while(1)
+   {
+   if(s%a==0&&s%a==0)
+   {
+   printf("The LCM of two num is  %d",s);
+   break;
+}   
+++s;
+}
+    return 0;
+    }
