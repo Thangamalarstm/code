33bf7235c514d510d125c59df2563654f1c4e79a
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-02-23 16:24:45 +0530

    Create array sorting.c

---
+++ array sorting.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+ int main()
+{
+int a[25],i,j,n,temp;
+printf("\n Enter the number..:");
+scanf("%d",&n);
+printf("\n Enter the elements..:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+ }
+  }
+    
+}
+
+   printf("\n The sorted array is...");
+  for(i=0;i<n;i++){
+    printf("%d\t",a[i]);
+}
+return 0;
+}
