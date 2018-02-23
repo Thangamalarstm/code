eb70d63b31d39834b414265cea3dc17cb9fc5872
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-02-23 16:46:02 +0530

    Create cuboid volume.c

---
+++ cuboid volume.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+float a,b,c,d;
+printf("\n Length of an area");
+scanf("%f",&a);
+printf("\n Width of an area");
+scanf("%f",&b);
+printf("\n Height of an area");
+scanf("%f",&c);
+d=a*b*c;
+printf(" volume of cubiod is..:%5f",d);
+return 0;
+}
