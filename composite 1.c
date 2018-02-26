5d31486e73572f993748a5c177fb5792133f55d8
Thangamalarstm <30549354+Thangamalarstm@users.noreply.github.com>
2018-02-26 16:36:54 +0530

    Create composite 1.c

---
+++ composite 1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int c=0,l,i;
+printf("/n Enter the two number");
+scanf("%d",&l);
+for(i=2;i<=l/2;++i)
+{
+if(l%i==0)
+{
+c=1;
+break;
+}
+}
+if(c==0)
+{
+printf("NO");
+++l;
+}
+else
+{
+printf("YES,It is composite");
+
+}
+return 0;
+}
