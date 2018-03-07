e4d451be825398393582f28fa12f1fe207cf589c
Thangamalar Sakthivel <30549354+Thangamalarstm@users.noreply.github.com>
2018-03-07 15:51:05 +0530

    Create combain 3 digits.c

---
+++ combain 3 digits.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter the 3 digit number");
+scanf("%d",&a);
+printf("Enter the 3 digit number");
+scanf("%d",&b);
+c=(1000*a)+b;
+printf("the joint number is..:%d",c);
+return 0;
+}
