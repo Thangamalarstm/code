80bef87831b3a786ab26875138558d759a94ca7a
Thangamalar Sakthivel <30549354+Thangamalarstm@users.noreply.github.com>
2018-03-14 16:38:03 +0530

    Update b2wprime.c

--- b2wprime.c
+++ b2wprime.c
@@ -1,14 +1,33 @@
-#include <stdio.h>
-int main(void)
+#include<stdio.h>
+#include<string.h>
+int main()
 {
- for (int i=2; i<10; i++)
- {
-  for (int j=2; j<i; j++)   
-  {
-    if (i == j)  
-      printf("%d\n",i);
-    else if (i%j == 0)
-      break;
-  }
- }
+int a,b,cnt=0,i,j,c=0;
+printf("Enter the 2 numbers..:");
+scanf("%d%d",&a,&b);
+
+for(i=a;i<b;i++)
+{
+    for(j=2;j<a;j++)
+{
+    if(i%j==0)
+    {
+        cnt=1;
+        break;
+    }
+    else
+    {
+        cnt=0;
+    }
+   
+}
+
+if(cnt==0)
+{
+c=c+1;
+
+}
+}
+printf("prime numbers are:%d",c);
+return 0;
 }
